5a9d932543602a88598dfea6977cc73e8cfd4783
Lamont Barnett <stoverlee29@gmail.com>
2025-05-24 21:11:00 -0400

    fixed icons

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import './index.scss'
+import Sigil from '../Sigil/index'
 
 const Sidebar = () => {
   return (
     <>
-      {/* Pinkisland home glyph */}
       <div className="interface-fragment top-left">
-        <a href="/" className="glyph">⧊</a>
+        <a href="/">
+          <img src={Sigil} className="sigil-icon" alt="" />
+        </a>
       </div>
 
-      {/* Portal direct link (secondary)
-      <div className="interface-fragment bottom-left">
-        <a href="/portal" className="glyph">⧉</a>
-      </div> */}
-
-      {/* Symbolic glyphs for external links */}
       <div className="interface-fragment top-right">
         <a href="https://github.com/sirmojosodope" target="_blank" rel="noreferrer" className="glyph">∴</a>
         <a href="https://linkedin.com/in/lamontbarnett" target="_blank" rel="noreferrer" className="glyph">⧉</a>
--- src/components/Sigil/index.jsx
+++ src/components/Sigil/index.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './index.scss'
 
-const SigilComponent = () => {
-  return <div className="sigil"></div>
+const Sidebar = () => {
+  return (
+    <>
+      {/* Sigil Home Button */}
+      <div className="interface-fragment top-left">
+        <a href="/" className="sigil" aria-label="Home" />
+      </div>
+
+      {/* Symbolic Glyph Links */}
+      <div className="interface-fragment top-right">
+        <a
+          href="https://github.com/sirmojosodope"
+          target="_blank"
+          rel="noreferrer"
+          className="glyph"
+        >
+          ∴
+        </a>
+        <a
+          href="https://linkedin.com/in/lamontbarnett"
+          target="_blank"
+          rel="noreferrer"
+          className="glyph"
+        >
+          ⧉
+        </a>
+      </div>
+    </>
+  )
 }
 
-export default SigilComponent
+export default Sidebar
